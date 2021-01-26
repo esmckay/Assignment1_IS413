8bc5112967942cd52d90980ec8bb8af7d6b18a85
esmckay <70922535+esmckay@users.noreply.github.com>
2021-01-26 12:59:23 -0700

    yes

--- wwwroot/js/loadoutInfo.js
+++
@@ -1,7 +0,0 @@
-﻿var submit = document.getElementById("submitButton");
-
-submit.addEventListener("click", function () {
-    alert("Primary:  " + document.getElementById("primary").value + ", Secondary:  " +
-        document.getElementById("secondary").value + ", Lethal:  " + document.getElementById("lethal").value +
-        ", Misc Comments:  " + document.getElementById("misc").value);
-});
